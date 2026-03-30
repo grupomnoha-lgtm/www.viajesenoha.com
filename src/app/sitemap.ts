c40f558932f12b949db6d8236e9e83b4099143b5
Viajes Enoha Admin <info@viajesenoha.com>
2026-03-30 23:41:52 +0100

    Optimize SEO: Meta tags, keywords, and sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,12 @@
+export default function Sitemap() {
+  const baseUrl = 'https://www.viajesenoha.com';
+  
+  return [
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 1,
+    },
+  ];
+}
